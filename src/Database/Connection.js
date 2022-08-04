73a426f8b1e2bb7c7df01b35f7a9d0d794e46d51
pedrofnseca <pedropereira.pp264@gmail.com>
2022-08-04 15:51:52 -0300

    refatorando conexão com o banco

--- src/Database/Connection.js
+++ src/Database/Connection.js
@@ -4,16 +4,11 @@ import mysql from 'mysql2/promise'
 
 async function connect(){
 
-  const DB_HOST = process.env.DB_HOST || 'localhost'
-  const DB_USER = process.env.DB_USER || 'root'
-  const DB_PWD = process.env.DB_PWD || ''
-  const DB_NAME = process.env.DB_NAME || 'biblioteca'
-
   const connection = await mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PWD, 
-    database: DB_NAME
+    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_USER: process.env.DB_USER || 'root',
+    DB_PWD: process.env.DB_PWD || '',
+    DB_NAME: process.env.DB_NAME || 'biblioteca'
   })
 
   return connection
