630d0dd701852a6cde6943ad0362183dec5fc1c7
Pedro Fonseca <97262778+pedroFnseca@users.noreply.github.com>
2022-08-10 19:17:00 -0300

    Alterando mensagem notFound

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,12 @@ import cataloguingController from './controllers/cataloguingController.js'
 
 const router = express.Router()
 
-
 router.use('/cataloguin', cataloguingController)
 
 router.use('*', (req, res) => {
     res.status(404).json({
         notfound: 'verify on github the endpoints',
-        by: '@blibion'
+        by: '@BibliON'
     })
 })
 
