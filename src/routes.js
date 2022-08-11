933091a3644e4df8e1af8c20cdc0b50a2d3778f6
pedrofnseca <pedropereira.pp264@gmail.com>
2022-08-11 14:01:51 -0300

    Adicionando rota 'librian'

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import express from "express"
 import books from './controllers/BookController.js'
+import librian from './controllers/LibrianController.js'
 
 const router = express.Router()
 
 router.use('/books', books)
+router.use('/librian', librian)
 
 router.use('*', (req, res) => {
     res.status(404).json({
