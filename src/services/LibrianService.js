d7e694c0fd7dfb85020d3c6f603db85ed80b1d77
pedrofnseca <pedrofnseca@gmail.com>
2022-08-10 22:24:26 -0300

    Service login

--- src/services/LibrianService.js
+++ src/services/LibrianService.js
@@ -1,19 +1,38 @@
 import db from '../Database/Connection.js'
 
-async function createLibrian(data){
+// Realiza o cadastro de voluntários
+async function createVoluntary(data){
   const conn = await db.connect()
 
   const {name, email, password} = data
 
-  const sql = 'INSERT INTO tbl_librian (librian_name, librian_email, librian_password) values (?, ?, ?)'
+  const sql = 'INSERT INTO tbl_librian (librian_name, librian_email, librian_password, librian_type) values (?, ?, ?, ?)'
 
-  const values = [name, email, password]
+  const values = [name, email, password, 'Colaborador']
 
   await conn.query(sql, values)
 
   conn.end()
 }
 
+// Realiza o login do Bibliotecario
+async function loginLibrian(data){
+  const conn = await db.connect()
+
+  const {email, password} = data
+
+  const sql = 'SELECT * From tbl_librian where librian_email = ? and librian_password = ?'
+
+  const values = [email, password]
+
+  const [rows] = await conn.query(sql, values)
+
+  conn.end()
+
+  return rows
+}
+
 export default{
-  createLibrian
+  createVoluntary,
+  loginLibrian
 }
