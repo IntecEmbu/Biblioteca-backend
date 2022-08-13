3d5e40d502b46c956b7554af76daf3c7737d2e71
pedrofnseca <pedropereira.pp264@gmail.com>
2022-08-12 21:02:24 -0300

    Adicionando rota de email

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import express from "express"
 import books from './controllers/BookController.js'
 import librian from './controllers/LibrianController.js'
+import Nodemailer from "./Mail/Nodemailer.js"
 
 const router = express.Router()
 
 router.use('/book', books)
 router.use('/librian', librian)
+router.use('/email', Nodemailer)
 
 router.use('*', (req, res) => {
     res.status(404).json({
