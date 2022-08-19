6a126608696bf595c68acfde2d3d4354ff827778
pedrofnseca <pedropereira.pp264@gmail.com>
2022-08-19 14:11:45 -0300

    Alterando variaveis de banco para .env

--- src/Database/Connection.js
+++ src/Database/Connection.js
@@ -2,10 +2,10 @@ import mysql from 'mysql2/promise'
 
 async function connect(){
 
-  const DB_HOST = process.env.DB_HOST || 'localhost'
-  const DB_USER = process.env.DB_USER || 'root'
-  const DB_PWD = process.env.DB_PWD || ''
-  const DB_NAME = process.env.DB_NAME || 'biblioteca'
+  const DB_HOST = process.env.DB_HOST
+  const DB_USER = process.env.DB_USER
+  const DB_PWD = process.env.DB_PWD 
+  const DB_NAME = process.env.DB_NAME
 
   const connection = await mysql.createConnection({
     host: DB_HOST,
