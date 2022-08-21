bd84e65d90d3e06c69bb7d236bbd11cece72fe46
pedrofnseca <pedropereira.pp264@gmail.com>
2022-08-20 21:29:08 -0300

    Adicionando rota lending

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import express from "express"
 import books from './controllers/BookController.js'
 import librian from './controllers/LibrianController.js'
 import user from './controllers/UserController.js'
+import lending from './controllers/LendingController.js'
 
 const router = express.Router()
 
 router.use('/book', books)
 router.use('/librian', librian)
 router.use('/user', user)
+router.use('/lending', lending)
 
 router.use('*', (req, res) => {
     res.status(404).json({
