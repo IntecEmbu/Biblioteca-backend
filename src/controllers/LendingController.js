31a02744f4493916b9200aef4a9d7681c438286d
pedrofnseca <pedropereira.pp264@gmail.com>
2022-08-20 21:27:12 -0300

    Rota de emprestimos

---
+++ src/controllers/LendingController.js
@@ -0,0 +1,91 @@
+import express from 'express'
+import db from '../services/LendingService.js'
+import { body, validationResult } from 'express-validator'
+
+const router = express.Router()
+
+// Endpoint: / (POST)
+// Descrição: Cadastra um emprestimo no banco de dados
+router.post('/',[
+    body('librian_id').not().isEmpty().withMessage('Librarian id is required'),
+    body('book_id').not().isEmpty().withMessage('Book id is required'),
+    body('user_id').not().isEmpty().withMessage('User id is required'),
+    body('withdraw_date').not().isEmpty().withMessage('Withdraw date is required')
+], async (req, res) => {
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+
+    const {librian_id, book_id, user_id, withdraw_date, return_date} = req.body
+
+    try {
+        await db.createLending({librian_id, book_id, user_id, withdraw_date, return_date})
+        res.status(200).json({
+            message: 'Lending created successfully'
+        })
+    } catch(error){
+        return res.status(500).json({
+            DatabaseError: error.message
+        })
+    }
+})
+
+// Endpoint: /return-book (UPDATE)
+// Descrição: Devolve um livro
+router.put('/return-book',[
+    body('lending_id').not().isEmpty().withMessage('Lending id is required'),
+    body('return_date').not().isEmpty().withMessage('Return date is required')
+], async (req, res) => {
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+
+    const {lending_id, return_date} = req.body
+
+    try {
+        await db.returnBook({lending_id, return_date})
+        res.status(200).json({
+            message: 'Book returned successfully'
+        })
+    } catch(error){
+        return res.status(500).json({
+            DatabaseError: error.message
+        })
+    }
+})
+
+// Endpoint: /not-returned (GET)
+// Descrição: Lista os emprestimos não devolvidos
+router.get('/not-returned', async (req, res) => {
+
+    try {
+        const result = await db.getNotReturned()
+
+        if(result.length > 0){
+            return res.status(204).json({
+                message: 'Not returned found',
+                data: result
+            })
+        }
+
+        res.status(200).json({
+            message: 'Not returned list',
+            data: result
+        })
+
+    } catch(error){
+        return res.status(500).json({
+            DatabaseError: error.message
+        })
+    }
+})
+
+export default router
--- src/services/LendingService.js
+++ src/services/LendingService.js
@@ -6,15 +6,45 @@ async function createLending(data){
 
     const conn = await db.connect()
 
-    const sql = 'INSERT INTO tbl_lending (FK_librarian, FK_book, FK_user, withdraw_date, return_date) values (?, ?, ?, ?, ?)'
+    const sql = 'INSERT INTO tbl_lending (FK_librarian, FK_book, FK_user, withdraw_date) values (?, ?, ?, ?)'
 
-    const values = [librian_id, book_id, user_id, withdraw_date, return_date]
+    const values = [librian_id, book_id, user_id, withdraw_date]
 
     await conn.query(sql, values)
 
     conn.end()
 }
 
+// Devolve um livro
+async function returnBook(data){
+    const {lending_id, return_date} = data
+
+    const conn = await db.connect()
+
+    const sql = 'UPDATE tbl_lending SET return_date = ? WHERE id = ?'
+
+    const values = [return_date, lending_id]
+
+    await conn.query(sql, values)
+
+    conn.end()
+}
+
+// Retorna todos os emprestimos não devolvidos
+async function getAllNotReturned(){
+    const conn = await db.connect()
+
+    const sql = 'SELECT * FROM tbl_lending WHERE return_date IS NULL'
+
+    const [rows] = await conn.query(sql)
+
+    conn.end()
+
+    return rows
+}
+
 export default{
-    createLending
+    createLending,
+    returnBook,
+    getAllNotReturned
 }
