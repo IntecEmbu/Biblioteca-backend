e2282b13fb1c217a350b9135860d4dcd3b8051d1
pedrofnseca <pedropereira.pp264@gmail.com>
2022-09-03 00:31:45 -0300

    add hifen

--- src/email/welcomeEmail.js
+++ src/email/welcomeEmail.js
@@ -2,7 +2,7 @@ import emailTemplate from './template.js'
 
 function welcomeEmail(name){
     return emailTemplate(
-        'Bem vindo(a)',
+        'Bem-vindo(a)',
         name,
         'Agora você pode realizar empréstimos e devoluções de livros utilizando o sistema Biblitec. Aproveite!'
     )
