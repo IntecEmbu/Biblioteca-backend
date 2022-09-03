460ae3b26afc6270193d30d753c33f596ca92e6a
pedrofnseca <pedropereira.pp264@gmail.com>
2022-09-03 13:26:32 -0300

    Tentativa de arrumar bug

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -6,7 +6,9 @@ async function createUser(data){
 
     const conn = await db.connect()
 
-    const sql = `INSERT INTO tbl_user (user_name, user_email, user_type, user_phone, user_course, user_status), values (?, ?, ?, ?, ?, 'Ativo')`
+    const sql = `INSERT INTO tbl_user 
+    (user_name, user_email, user_type, user_phone, user_course, user_status) 
+        value (?, ?, ?, ?, ?, 'Ativo')`
 
     const values = [name, email, type, phone, course]
 
