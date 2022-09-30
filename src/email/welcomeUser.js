2808b47f2518abfe3cd8974ba25f162f3871549f
Pedro Fonseca <97262778+pedroFnseca@users.noreply.github.com>
2022-09-29 22:06:35 -0300

    Alterando mensagem

--- src/email/welcomeUser.js
+++ src/email/welcomeUser.js
@@ -3,9 +3,9 @@ import sendEmail from '../services/sendEmail.js'
 
 async function welcomeUser(name, to){
     const html = emailTemplate(
-        'Bem-vindo(a)',
+        'Bem-vindo(a)!',
         name,
-        'Agora você pode realizar empréstimos e devoluções de livros utilizando o sistema Biblitec. Aproveite!'
+        'Agora você pode pegar livros emprestados na biblioteca da ETEC de Embu através do sistema BibliTec.'
     )
     
     const subject = 'Boas vindas!'
