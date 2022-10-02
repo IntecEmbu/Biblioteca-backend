77990e97a8389a27ace42f3b8ca357fca5f7211c
pedrofnseca <pedropereira.pp264@gmail.com>
2022-10-02 18:19:15 -0300

    Fix bug update user

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -49,8 +49,7 @@ async function updateUser(data) {
   const conn = await db.connect();
 
   const sql =
-    "UPDATE tbl_user SET user_name = ?, user_email = ?, user_type = ?, user_phone = ? user_course = ?, user_cpf = ? WHERE user_code = ?";
-
+    "UPDATE tbl_user SET user_name = ?, user_email = ?, user_type = ?, user_phone = ?, user_course = ?, user_cpf = ? WHERE user_code = ?";
   const values = [name, email, type, phone, course, id, cpf];
 
   await conn.query(sql, values);
