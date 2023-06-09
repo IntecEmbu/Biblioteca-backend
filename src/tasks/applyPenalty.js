13643287c20a4a85f1d32197adfeded4f41b13f2
pedrofnseca <pedropereira.pp264@gmail.com>
2023-06-08 22:34:27 -0300

    :zap: application performance

--- src/tasks/applyPenalty.js
+++ src/tasks/applyPenalty.js
@@ -17,11 +17,16 @@ export default async function(){
   const lendings = await getOverdue();
 
   if (lendings.length === 0) return;
-
+  
   const conn = await db.connect();
-  for (const lending of lendings) {
-    await conn.query("CALL SP_lending_penalty(?)", lending.lending_code);
-  }
 
-  conn.end();
+  try {
+    for (const lending of lendings) {
+      await conn.query("CALL SP_lending_penalty(?)", lending.lending_code);
+    }
+  } catch (error) {
+    // console.error(error)
+  } finally {
+    conn.end();
+  }
 }
